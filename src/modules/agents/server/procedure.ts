b0e109de2db2b91a95746aceb3acc4c738cc9f10
Radhey Mugdal <mugdalradhey@gmail.com>
2025-06-06 23:34:24 +0530

    feat: create data table to show all agents

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -7,18 +7,25 @@ import {
 } from "@/trpc/init";
 import { agentInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getOne: baseProcedure
+  getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input, ctx }) => {
       const { id } = input;
-      const [data] = await db.select().from(agents).where(eq(agents.id, id));
+      const [data] = await db
+        .select({
+          ...getTableColumns(agents),
+          meetingCount: sql<number>`5`,
+        })
+        .from(agents)
+        .where(eq(agents.id, id));
       return data;
     }),
-  getMany: baseProcedure.query(async () => {
-    return [];
+  getMany: protectedProcedure.query(async () => {
+    const data = await db.select().from(agents);
+    return data;
   }),
   create: protectedProcedure
     .input(agentInsertSchema)
