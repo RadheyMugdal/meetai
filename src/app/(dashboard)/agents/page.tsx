337ec9e926db0e6a75c0ab4a34a893285b3820e3
Radhey Mugdal <mugdalradhey@gmail.com>
2025-06-07 21:30:58 +0530

    feat: create pagination and filter for agents table

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -7,10 +7,21 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const AgentsPage = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const AgentsPage = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
   return (
     <>
       <ListHeader />
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -10,16 +10,27 @@ import { DataTable } from "../components/data.table";
 import { columns } from "../components/colums";
 import EmptyState from "@/components/empty-state";
 import { AgentGetOne } from "../../types";
+import { useAgentsFilters } from "../../hooks/use-agents-filter";
+import DataPagination from "../components/data-pagination";
 
 const AgentsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  console.log(data);
+  const [filters, setFilters] = useAgentsFilters();
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
   return (
     <div className=" flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data as AgentGetOne[]} columns={columns} />
-      {data.length === 0 && (
+      <DataTable data={data.items} columns={columns} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
         <EmptyState
           title="Create you first agent"
           description="Create an agent to join your meetings. Each agent will follow your instructions."
@@ -28,6 +39,7 @@ const AgentsView = () => {
     </div>
   );
 };
+``;
 
 export default AgentsView;
 
