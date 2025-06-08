bd8bdae6ae7eb10814f89a35d594fbf0beecad62
Radhey Mugdal <mugdalradhey@gmail.com>
2025-06-08 16:30:34 +0530

    feat: add agent/id page to manage individual agent

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -12,8 +12,10 @@ import EmptyState from "@/components/empty-state";
 import { AgentGetOne } from "../../types";
 import { useAgentsFilters } from "../../hooks/use-agents-filter";
 import DataPagination from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 const AgentsView = () => {
+  const router = useRouter();
   const trpc = useTRPC();
   const [filters, setFilters] = useAgentsFilters();
   const { data } = useSuspenseQuery(
@@ -24,7 +26,11 @@ const AgentsView = () => {
 
   return (
     <div className=" flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
