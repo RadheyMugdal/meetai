0be4c4e1c41fd44ca4e624211e325ab5f1db2b6a
Radhey Mugdal <mugdalradhey@gmail.com>
2025-06-13 22:50:12 +0530

    feat: Create Meetings table to see meetings in "/meetings" route

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
@@ -47,8 +47,17 @@ export const meetingsRouter = createTRPCRouter({
       const data = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`
+                    CASE
+                      WHEN "meetings"."ended_at" IS NOT NULL AND "meetings"."started_at" IS NOT NULL
+                      THEN EXTRACT(EPOCH FROM "meetings"."ended_at" - "meetings"."started_at")  
+                      ELSE 0
+                    END
+                  `.as("duration"),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
@@ -62,6 +71,7 @@ export const meetingsRouter = createTRPCRouter({
       const [total] = await db
         .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
